dc2aef4aa3bc92940a238e0801e6d02f833a4c10
chirpthetwerp <32555127+chirpthetwerp@users.noreply.github.com>
2018-06-07 17:33:05 -0400

    Add files via upload

--- me/abhi/hook/commands/VoteCommand.java
+++ me/abhi/hook/commands/VoteCommand.java
@@ -7,13 +7,19 @@ import me.abhi.hook.util.Messages;
 
 public class VoteCommand extends BukkitCommand {
 
-    public VoteCommand() {
-        super("vote");
-    }
+	public VoteCommand() {
+		super("vote");
+		setPermission("hook.command.vote");
+		setPermissionMessage(Messages.NO_PERMISSION);
+	}
 
-    @Override
-    public  boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        sender.sendMessage(Messages.VOTE_MESSAGE);
-        return true;
-    }
+	@Override
+	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+		if (!sender.hasPermission(this.getPermission())) {
+			sender.sendMessage(getPermissionMessage());
+			return true;
+		}
+		sender.sendMessage(Messages.VOTE_MESSAGE);
+		return true;
+	}
 }
